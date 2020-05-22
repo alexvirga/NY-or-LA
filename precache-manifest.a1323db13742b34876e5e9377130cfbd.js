d9b705186e08e6c1f8e59274780094829f32aab8
alexvirga <alexander.virga@gmail.com>
2020-05-22 02:01:19 -0400

    Updates

---
+++ precache-manifest.a1323db13742b34876e5e9377130cfbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d436274f9d13678fd8006464e8ae9d",
+    "url": "/NY-or-LA/index.html"
+  },
+  {
+    "revision": "76e9cb28ab0198be350c",
+    "url": "/NY-or-LA/static/css/main.3fc80c10.chunk.css"
+  },
+  {
+    "revision": "e7efaaa286fcb6722fcc",
+    "url": "/NY-or-LA/static/js/2.e71b264d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NY-or-LA/static/js/2.e71b264d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e9cb28ab0198be350c",
+    "url": "/NY-or-LA/static/js/main.bc601437.chunk.js"
+  },
+  {
+    "revision": "d393641500ef4c136892",
+    "url": "/NY-or-LA/static/js/runtime-main.3b05072c.js"
+  }
+]);
